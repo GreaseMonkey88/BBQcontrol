892b0a12305d3969c05a88e1b966e58c1044ee95
GreaseMonkey88 <47741656+GreaseMonkey88@users.noreply.github.com>
2020-11-15 13:30:13 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;
+
+int incomingByte = 0; // for incoming serial data
+int pos = 90;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myservo.attach(9);
+  myservo.write(pos);
+  delay(50);
+  Serial.println(pos);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    if (incomingByte == 43) {
+      pos = pos + 5;
+      myservo.write(pos);
+      Serial.println(pos);
+    }
+
+    if (incomingByte == 45) {
+      pos = pos - 5;
+      myservo.write(pos);
+      Serial.println(pos);
+    }
+  }
+
+
+
 }
