9e8adfc2fb9da7e4eee4a3b1553a78c7ad1cdc23
GreaseMonkey88 <47741656+GreaseMonkey88@users.noreply.github.com>
2020-11-17 09:50:37 +0100

    base working

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,159 @@
-#include <Arduino.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Servo.h>
 
-Servo myservo;
+#define DEVmessages 1 // change 1 to 0 if you don´t want MQTT DEV messages
 
+// Wifi and mqtt network settings
+const char *ssid = "the dude-net";
+const char *password = "iR3DNw8ZFk-t9e3ixVJjhAE-2d9374H9sw5-Sv99fC645C2-6G4359L463tY";
+const char *HostName = "ESP8266-BBQ"; // Edit the hostname which will be shown in your LAN
+const char *mqtt_server = "10.0.0.10";
+const char *mqtt_user = "mark";
+const char *mqtt_pass = "8749";
+const char *SensorName = "BBQcontrol";
+const char *NTPserver = "10.0.0.1";
+const char *version = "BBQcontrol v1.00";
+
+// Other variables
+volatile long unsigned counter;
+volatile long unsigned lastTime;
 int incomingByte = 0; // for incoming serial data
 int pos = 90;
+int hyterese = 5;
+int lastDir = 2;
+int stepServo;
 
-void setup() {
-  Serial.begin(9600);
-  myservo.attach(9);
-  myservo.write(pos);
-  delay(50);
-  Serial.println(pos);
+// NTP stuff
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTPserver, 3600, 600000);
+
+Servo myservo;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi()
+{
+  delay(100);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.hostname(HostName);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // send data only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
 
-    if (incomingByte == 43) {
-      pos = pos + 5;
-      myservo.write(pos);
-      Serial.println(pos);
+    if (client.connect(SensorName, mqtt_user, mqtt_pass))
+    {
+      client.subscribe("BBQcontrol/p_step");
+      client.subscribe("BBQcontrol/control");
+      Serial.println("connected");
     }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
 
-    if (incomingByte == 45) {
-      pos = pos - 5;
-      myservo.write(pos);
-      Serial.println(pos);
+      delay(6000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  String stringTopic = String(topic);
+  if (stringTopic.equals("BBQcontrol/p_step")) {
+    char stepServoString[6];
+    uint8_t pIdx;
+    for (pIdx = 0; pIdx < 5 && pIdx < length; pIdx++) {
+      stepServoString[pIdx] = payload[pIdx];
+    }
+    stepServoString[pIdx] = '\0';
+    
+    if (String(stepServoString).toInt() > 0) {
+      stepServo = String(stepServoString).toInt();
+      Serial.print("p_step: ");
+      Serial.println(stepServo);
     }
   }
 
+  // Evaluate first character received
+  if ((char)payload[0] == '+') {
+    if (lastDir == 0) {
+      pos = pos + hyterese;
+    }
+    pos = pos + stepServo;
+    myservo.write(pos);
+    client.publish("BBQcontrol/DEVposition", String(pos).c_str(), true);
+    lastDir = 1;
+    Serial.println(pos);
+    Serial.print("Step: ");
+    Serial.println(stepServo);
+  }
 
+  if ((char)payload[0] == '-') {
+    if (lastDir == 1) {
+      pos = pos - hyterese;
+    }
+    pos = pos - stepServo;
+    myservo.write(pos);
+    client.publish("BBQcontrol/DEVposition", String(pos).c_str(), true);
+    lastDir = 0;
+    Serial.println(pos);
+    Serial.print("Step: ");
+    Serial.println(stepServo);
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(" ");
+  Serial.println("Starting up...");
+  setup_wifi();
+  client.setServer(mqtt_server, 1885);
+  client.setCallback(callback);
+  timeClient.begin();
+  Serial.println(version);
+
+  myservo.attach(2);
+  myservo.write(pos);
+  delay(50);
+  Serial.println(pos);
+  stepServo = 5;
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+
+  client.loop();
+  timeClient.update();
 
 }
